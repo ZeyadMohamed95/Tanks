30e3ae934f520c307a25b3c532bdf5174b4e8e58
Zeyad <ZeyadMohamed959595@gmail.com>
2019-08-30 11:41:47 +0200

    first commit

---
+++ Assets/Scripts/PickUps/Picking.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Picking : MonoBehaviour {
+    private TankMovement m_tankMovement;
+    private bool isActive = false;
+    private MeshRenderer rend;
+
+    void Awake()
+    {
+        rend = GetComponentInChildren<MeshRenderer>(); 
+    }
+	// Use this for initialization
+    private void OnTriggerEnter(Collider other)
+    {
+        m_tankMovement = other.GetComponent<TankMovement>();
+        if (m_tankMovement != null && isActive == false)
+        {
+            isActive = true;
+            StartCoroutine(PowerUpWearOff(5.0f));
+        }
+        rend.enabled = false;
+    }
+    IEnumerator PowerUpWearOff(float waitTime)
+    {
+        m_tankMovement.m_Speed *= 2.0f;
+        yield return new WaitForSeconds(waitTime);
+        m_tankMovement.m_Speed *= 0.5f; // remove boost
+        
+        isActive = false;
+        //print("Coroutine ended: " + Time.time + " seconds");
+    }
+}
---
+++ Assets/Scripts/PickUps/randomspawn.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomspawn : MonoBehaviour {
+    public GameObject PickUP;
+    public float StartWait;
+
+    private Vector3 pos;
+    private MeshRenderer rend;
+	// Use this for initialization
+	void Start ()
+    {
+        rend = PickUP.GetComponentInChildren<MeshRenderer>(); 
+        rend.enabled = false;
+        
+	}
+	
+    void Update()
+    {
+        StartCoroutine(SpawningFunction());
+    }
+	
+	IEnumerator SpawningFunction()
+    {
+        yield return new WaitForSeconds(StartWait);
+        if (rend.enabled==false)
+        {
+            rend.enabled = true;
+            print("Coroutine ended: ");
+            // x between -40 and 40 and z between -40 and 40 y = .6
+            pos = new Vector3(Random.Range(-40f, 40f), 0.6f, Random.Range(-40f, 40f));
+            PickUP.transform.position = pos;
+        }
+
+    }
+}
